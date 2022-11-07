ac7112f9a0d59f5e8a09352ef2febea7add5e470
buddle6091 <83018778+buddle6091@users.noreply.github.com>
2022-11-07 16:31:57 +0900

    styled components 적용

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
 
 interface caro {
   ImgUrl: string;
@@ -17,20 +18,47 @@ const caroArray: Array<caro> = [
     ImgUrl:
       "https://images.unsplash.com/photo-1603052227529-e8ed43c7af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
   },
+  {
+    ImgUrl:
+      "https://images.unsplash.com/photo-1602808180309-2e0c62986635?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    ImgUrl:
+      "https://images.unsplash.com/photo-1583434987437-1b9dcbe44c9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    ImgUrl:
+      "https://images.unsplash.com/photo-1603052227529-e8ed43c7af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    ImgUrl:
+      "https://images.unsplash.com/photo-1602808180309-2e0c62986635?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    ImgUrl:
+      "https://images.unsplash.com/photo-1583434987437-1b9dcbe44c9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    ImgUrl:
+      "https://images.unsplash.com/photo-1603052227529-e8ed43c7af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
 ];
+
+/* const leftRef = useRef();
+const rightRef = useRef(); */
 
 function Carousel() {
   return (
     <div className="box-border">
-      <div className="w-[500px] h-[300px] overflow-hidden">
-        <div className="flex">
+      <CarouselBox>
+        <CarouselInline>
           {caroArray.map((x, i) => (
             <div key={x.ImgUrl}>
-              <img src={x?.ImgUrl} className="w-[500px] h-[300px]" />
+              <CarouselImg src={x?.ImgUrl} alt={x?.ImgUrl} />
             </div>
           ))}
-        </div>
-      </div>
+        </CarouselInline>
+      </CarouselBox>
       <button id="left"> 왼 </button>
       <button id="right"> 오 </button>
     </div>
@@ -38,3 +66,19 @@ function Carousel() {
 }
 
 export default Carousel;
+
+const CarouselBox = styled.div`
+  width: 500px;
+  height: 300px;
+  overflow: hidden;
+`;
+
+const CarouselInline = styled.div`
+  display: inline-block;
+`;
+
+const CarouselImg = styled.img`
+  width: 500px;
+  height: 300px;
+  flex: none;
+`;
