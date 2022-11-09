b9296437cfce8b733b6e95c637083782392df85f
buddle6091 <83018778+buddle6091@users.noreply.github.com>
2022-11-09 23:36:03 +0900

    util/DragEvent

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Carousel />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-3xl font-bold underline-offset-1">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, MouseEvent } from "react";
 import axios from "axios";
 import styled from "styled-components";
 
@@ -9,48 +9,30 @@ interface caro {
 const caroArray: Array<caro> = [
   {
     ImgUrl:
-      "https://images.unsplash.com/photo-1602808180309-2e0c62986635?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      "https://blog.kakaocdn.net/dn/dpxiAT/btqUBv6Fvpn/E8xUMncq7AVuDeOim0LrMk/img.jpg",
   },
   {
     ImgUrl:
-      "https://images.unsplash.com/photo-1583434987437-1b9dcbe44c9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      "https://blog.kakaocdn.net/dn/BGT7X/btqUzvTqi5h/flp39GdJH0GU6mo7cTbbhk/img.jpg",
   },
   {
     ImgUrl:
-      "https://images.unsplash.com/photo-1603052227529-e8ed43c7af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      "https://blog.kakaocdn.net/dn/bWnmfv/btqUBwqZvwA/3CiXGt3SR0TXoOveRJxV91/img.jpg",
   },
   {
     ImgUrl:
-      "https://images.unsplash.com/photo-1602808180309-2e0c62986635?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      "https://blog.kakaocdn.net/dn/XsLCO/btqUL8PQLwp/NZWCU2jAYKkKSXwcohBKTK/img.jpg",
   },
   {
     ImgUrl:
-      "https://images.unsplash.com/photo-1583434987437-1b9dcbe44c9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    ImgUrl:
-      "https://images.unsplash.com/photo-1603052227529-e8ed43c7af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    ImgUrl:
-      "https://images.unsplash.com/photo-1602808180309-2e0c62986635?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    ImgUrl:
-      "https://images.unsplash.com/photo-1583434987437-1b9dcbe44c9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    ImgUrl:
-      "https://images.unsplash.com/photo-1603052227529-e8ed43c7af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      "https://blog.kakaocdn.net/dn/bG3iVL/btqUvCZPaRL/ofIjkNWJP1mj2bOG9fie51/img.jpg",
   },
 ];
 
-/* const leftRef = useRef();
-const rightRef = useRef(); */
-
-/* const [currentIdx, setCurrentIdx] = useState(0); */
+/* 해당 Carousel item의 현재 index */
 
 function Carousel() {
+  /* goCamp API 출력 확인 */
   useEffect(() => {
     async function a() {
       const res = await axios.get(
@@ -61,49 +43,92 @@ function Carousel() {
     a();
   }, []);
 
+  /* 현재 캐로셀 아이템의 인덱스 값 */
+  const [currentIdx, setCurrentIdx] = useState(0);
+
+  /* 자동 슬라이드 기능  --> 일단 보류 */
+  /* const autoSlide = (callback, delay) => {
+    const itemRef = useRef();
+    useEffect(() => {
+      itemRef.current = callback;
+    }, [callback]);
+
+    useEffect(() => {
+      const ex = () => {
+        itemRef.current()
+      }
+      if (delay !== null) {
+        let id = setInterval(ex, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay])
+  } */
+
+  /* 해당 Carousel item 이 잘 찍혔는지 확인 */
+  /*   const handleClick =
+    (currentIdx: number) => (event: MouseEvent<HTMLElement>) => {
+      console.log(currentIdx + "event target is : ", event);
+    };
+
+  const cycler = (cycle: number) => (num: number) => {
+    ((num % cycle) + cycle) % cycle;
+  }; */
+
+  /* 왼쪽으로 넘기기 */
+  /*   const swipeLeft = () => {
+    setCurrentIdx((cur) => cycler())
+  }
+ */
+
+  /* 오른쪽으로 넘기기 */
+
   return (
-    <CarouselBox>
-      <CarouselUl>
-        {caroArray.map((x, i) => (
-          <CarouselLi key={x.ImgUrl}>
+    <CarouselViewer>
+      {/* currentIdx 값에 따라 x축 방향으로 이동하기 위하여 style 적용한다.
+      Carousel item을 왼쪽으로 옮겨 숨겨진 item을 오른쪽에서 가져와야되기 때문에 left로 가기 위해 음수의 값을 가져야 됨 
+      Carousel은 item 개수 만큼의 width을 가진다. 하나의 슬라이드 = total Carousel width / items.length*/}
+      <CarouselSlider
+        style={{
+          transform: `translateX(${
+            (-100 / caroArray.length) * (0.5 + currentIdx)
+          }%)`,
+        }}>
+        {caroArray.map((x) => (
+          <CarouselSlide key={x.ImgUrl}>
             <CarouselImg src={x?.ImgUrl} alt={x?.ImgUrl} />
-          </CarouselLi>
+          </CarouselSlide>
         ))}
-      </CarouselUl>
-    </CarouselBox>
+      </CarouselSlider>
+    </CarouselViewer>
   );
 }
 
 export default Carousel;
 
-const CarouselBox = styled.div`
-  width: 100%;
-  height: 300px;
-  display: flex;
+const CarouselViewer = styled.div`
+  width: 600px;
+  height: 400px;
   overflow: hidden;
 `;
 
-const CarouselUl = styled.ul`
-  width: 500px;
-  height: 300px;
+const CarouselSlider = styled.div`
   display: flex;
-  position: relative;
 `;
 
-const CarouselLi = styled.li`
-  list-style: none;
+const CarouselSlide = styled.div`
   width: 500px;
   height: 300px;
-  display: flex;
+  flex-shrink: 0;
+  display: block;
   position: relative;
-  overflow: hidden;
 `;
 
 const CarouselImg = styled.img`
-  width: 300px;
-  height: 300px;
+  width: 400px;
+  height: 400px;
   padding-left: 10px;
   border-radius: 1px solid;
   object-fit: contain;
+  transition: all 0.3s ease;
   flex: none;
 `;
